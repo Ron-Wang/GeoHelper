541b0252b768ec5cf9ec3bd9aaae7878664bfbd5
王荣鑫 <1499132850@qq.com>
2017-08-27 21:29:44 +0800

    Update GeoDbOpenHelper.java

--- Geology_Helper1.1/app/src/main/java/com/thutyh/geology_helper11/GeoDbOpenHelper.java
+++ Geology_Helper1.1/app/src/main/java/com/thutyh/geology_helper11/GeoDbOpenHelper.java
@@ -8,17 +8,15 @@ public class GeoDbOpenHelper extends SQLiteOpenHelper {
 
     public GeoDbOpenHelper(Context context, String name , SQLiteDatabase.CursorFactory factory, int version){
         super(context,name,factory,version);
-        //TODO Auto-generated constructor stub
     }
 
     @Override
     public void onCreate(SQLiteDatabase db){
-        //TODO Auto-generated method stub
-
+        
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-        //TODO Auto-generated method stub
+        
     }
 }
