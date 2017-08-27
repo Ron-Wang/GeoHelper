6112f1796491d65cf6a9db8db1651eb58c4ef8c2
王荣鑫 <1499132850@qq.com>
2017-08-27 21:26:47 +0800

    Update CallActivity.java

--- Geology_Helper1.1/app/src/main/java/com/thutyh/geology_helper11/CallActivity.java
+++ Geology_Helper1.1/app/src/main/java/com/thutyh/geology_helper11/CallActivity.java
@@ -18,7 +18,7 @@ public class CallActivity extends AppCompatActivity {
         setContentView(R.layout.activity_call);
 
         txtCall = (TextView)findViewById(R.id.TxtCall);
-        txtCall.setText("        GeoHelper(地质助手)是一款用于野外测量结构面产状、数据存储导出、进行统计绘图以及节理模拟的Android软件。\n        本APP由清华大学水利系徐文杰老师指导、王荣鑫带领的小组进行开发测试，目前尚处于1.0版本，还有很多需要完善的地方。\n        下面是相关的联系方式：\n地址：\n北京市海淀区清华大学水利系岩土所\n电话：\n010-62782301\n邮箱：\n徐文杰：xwjljh@126.com\n王荣鑫：wangrongxin168@163.com");
+        txtCall.setText("        GeoHelper(地质助手)是一款用于野外测量结构面产状、数据存储导出、进行统计绘图以及楔形体稳定系数计算的Android软件。\n        本APP由清华大学水利系徐文杰老师指导、王荣鑫带领的小组进行开发测试，目前尚处于1.0版本，还有很多需要完善的地方。\n        下面是相关的联系方式：\n地址：\n北京市海淀区清华大学水利系岩土所\n电话：\n010-62782301\n邮箱：\n徐文杰：xwjljh@126.com\n王荣鑫：wangrongxin168@163.com");
         btnCall1 = (Button)findViewById(R.id.btnCall1);
         btnCall2 = (Button)findViewById(R.id.btnCall2);
 
